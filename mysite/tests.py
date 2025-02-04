4a683811e16fd1ff469347bd65cdfa10ce83512d
one-tenth <11056010@ntub.edu.tw>
2025-02-04 16:22:33 +0800

    0204

--- mysite/tests.py
+++ mysite/tests.py
@@ -1,14 +1,14 @@
-# import cv2
-# from pyzbar.pyzbar import decode
+import cv2
+from pyzbar.pyzbar import decode
 
-# image = cv2.imread('static/IMG_0498.JPG')
-# gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# decoded_objects = decode(gray_image)
+image = cv2.imread('static/IMG_0498.JPG')
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+decoded_objects = decode(gray_image)
 
 
 
-# for obj in decoded_objects:
-#     print('Type:', obj.type)
-#     print('Data:', obj.data.decode('utf-8'))
+for obj in decoded_objects:
+    print('Type:', obj.type)
+    print('Data:', obj.data.decode('utf-8'))
 
 
